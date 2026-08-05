e635367ea5cd673fc9a8b4fd5abfc44f7488a609
himanshu201031 <himanshu.bisai7075@gmail.com>
2026-08-05 20:19:44 +0530

    fix: update import path for AuthContext in Login component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
+import { AuthContext } from '../context/authContext';
 import { useNavigate, Link } from 'react-router-dom';
 import { FaLock, FaEnvelope, FaShieldAlt } from 'react-icons/fa';
 import { FaArrowUpRightFromSquare } from 'react-icons/fa6';
